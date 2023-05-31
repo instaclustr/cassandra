5bca5d2b044fecb4324121d5a8f28bbde1c292a5
Stefan Miklosovic <smiklosovic@apache.org>
2023-05-31 23:21:08 +0200

    test

--- test/unit/org/apache/cassandra/config/YamlConfigurationLoaderTest.java
+++ test/unit/org/apache/cassandra/config/YamlConfigurationLoaderTest.java
@@ -33,6 +33,7 @@ import org.junit.Test;
 
 import org.apache.cassandra.distributed.shared.WithProperties;
 import org.apache.cassandra.io.util.File;
+import org.apache.cassandra.schema.CompressionParams;
 import org.yaml.snakeyaml.error.YAMLException;
 
 import static org.apache.cassandra.config.CassandraRelevantProperties.CONFIG_ALLOW_SYSTEM_PROPERTIES;
@@ -41,6 +42,8 @@ import static org.apache.cassandra.config.YamlConfigurationLoader.SYSTEM_PROPERT
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 
@@ -165,6 +168,23 @@ public class YamlConfigurationLoaderTest
 
         assertThat(c.row_index_read_size_warn_threshold).isEqualTo(new DataStorageSpec.LongBytesBound(1 << 12, KIBIBYTES));
         assertThat(c.row_index_read_size_fail_threshold).isEqualTo(new DataStorageSpec.LongBytesBound(1 << 13, KIBIBYTES));
+    }
+
+    @Test
+    public void readSSTableCompressionFromConfig()
+    {
+        Config c = load("test/conf/cassandra.yaml");
+
+        assertNull(c.sstable_compression);
+
+        c = load("test/conf/cassandra-with-sstable-compressor.yaml");
+
+        assertNotNull(c.sstable_compression);
+        assertThat(c.sstable_compression.class_name).isEqualTo("lz4");
+        assertThat(c.sstable_compression.parameters.remove(CompressionParams.CHUNK_LENGTH)).isEqualTo("32MiB");
+        assertThat(c.sstable_compression.parameters.remove(CompressionParams.MIN_COMPRESS_RATIO)).isEqualTo("1.5");
+        assertThat(c.sstable_compression.parameters.remove(CompressionParams.ENABLED)).isNull();
+        assertThat(c.sstable_compression.parameters.size()).isEqualTo(2);
     }
 
     @Test
