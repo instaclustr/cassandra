de640926bdd2f036107caf0071a7029e64d81f89
Stefan Miklosovic <smiklosovic@apache.org>
2023-07-19 12:01:14 +0200

    exclude system tables and fix bug with default compressor when setting enabled to true

--- src/java/org/apache/cassandra/db/ColumnFamilyStore.java
+++ src/java/org/apache/cassandra/db/ColumnFamilyStore.java
@@ -2953,7 +2953,7 @@ public class ColumnFamilyStore implements ColumnFamilyStoreMBean, Memtable.Owner
     {
         try
         {
-            TableParams.builder().crcCheckChance(crcCheckChance).build().validate();
+            TableParams.builder().crcCheckChance(crcCheckChance).build(keyspace.getName(), name).validate();
             for (ColumnFamilyStore cfs : concatWithIndexes())
             {
                 cfs.crcCheckChance.set(crcCheckChance);
