59bc2c723cad1408c45df07898c1d99d7cb72d41
Stefan Miklosovic <smiklosovic@apache.org>
2023-11-20 20:55:50 +0100

    CASSANDRA-18935 followup

--- src/java/org/apache/cassandra/service/CassandraDaemon.java
+++ src/java/org/apache/cassandra/service/CassandraDaemon.java
@@ -827,7 +827,6 @@ public class CassandraDaemon
     {
         if (nativeTransportService != null)
         {
-            StorageService.instance.setRpcReady(false);
             nativeTransportService.stop();
         }
     }
