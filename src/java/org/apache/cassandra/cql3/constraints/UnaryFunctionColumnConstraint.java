87f11a38d36ea80eb4147a88832347dc87b2378c
Stefan Miklosovic <smiklosovic@apache.org>
2025-02-04 15:19:29 +0100

    Implement NOT_NULL constraint
    patch by Stefan Miklosovic; reviewed by Bernardo Botella, TBD for CASSANDRA-20276
    
    Co-authored-by: Bernardo Botella Corbi <bernardo.botella@apple.com>

---
+++ src/java/org/apache/cassandra/cql3/constraints/UnaryFunctionColumnConstraint.java
@@ -0,0 +1,178 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.cql3.constraints;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.function.Function;
+
+import org.apache.cassandra.cql3.ColumnIdentifier;
+import org.apache.cassandra.cql3.CqlBuilder;
+import org.apache.cassandra.db.TypeSizes;
+import org.apache.cassandra.db.marshal.AbstractType;
+import org.apache.cassandra.io.util.DataInputPlus;
+import org.apache.cassandra.io.util.DataOutputPlus;
+import org.apache.cassandra.schema.ColumnMetadata;
+import org.apache.cassandra.tcm.serialization.MetadataSerializer;
+import org.apache.cassandra.tcm.serialization.Version;
+import org.apache.cassandra.utils.LocalizeString;
+
+import static org.apache.cassandra.cql3.constraints.ColumnConstraint.ConstraintType.UNARY_FUNCTION;
+
+public class UnaryFunctionColumnConstraint implements ColumnConstraint<UnaryFunctionColumnConstraint>
+{
+    public static final Serializer serializer = new Serializer();
+
+    public final ConstraintFunction function;
+    public final ColumnIdentifier columnName;
+
+    public final static class Raw
+    {
+        public final ConstraintFunction function;
+        public final ColumnIdentifier columnName;
+
+        public Raw(ColumnIdentifier functionName, ColumnIdentifier columnName)
+        {
+            this.columnName = columnName;
+            function = createConstraintFunction(functionName.toCQLString(), columnName);
+        }
+
+        public UnaryFunctionColumnConstraint prepare()
+        {
+            return new UnaryFunctionColumnConstraint(function, columnName);
+        }
+    }
+
+    private enum Functions
+    {
+        NOT_NULL(NotNullConstraint::new);
+
+        private final Function<ColumnIdentifier, ConstraintFunction> functionCreator;
+
+        Functions(Function<ColumnIdentifier, ConstraintFunction> functionCreator)
+        {
+            this.functionCreator = functionCreator;
+        }
+    }
+
+    private static ConstraintFunction createConstraintFunction(String functionName, ColumnIdentifier columnName)
+    {
+        try
+        {
+            return Functions.valueOf(LocalizeString.toUpperCaseLocalized(functionName)).functionCreator.apply(columnName);
+        }
+        catch (IllegalArgumentException ex)
+        {
+            throw new InvalidConstraintDefinitionException("Unrecognized constraint function: " + functionName);
+        }
+    }
+
+    private UnaryFunctionColumnConstraint(ConstraintFunction function, ColumnIdentifier columnName)
+    {
+        this.function = function;
+        this.columnName = columnName;
+    }
+
+    @Override
+    public MetadataSerializer<UnaryFunctionColumnConstraint> serializer()
+    {
+        return serializer;
+    }
+
+    @Override
+    public void appendCqlTo(CqlBuilder builder)
+    {
+        builder.append(toString());
+    }
+
+    @Override
+    public void evaluate(AbstractType<?> valueType, ByteBuffer columnValue) throws ConstraintViolationException
+    {
+        function.evaluate(valueType, columnValue);
+    }
+
+    @Override
+    public void validate(ColumnMetadata columnMetadata) throws InvalidConstraintDefinitionException
+    {
+        function.validate(columnMetadata);
+    }
+
+    @Override
+    public ConstraintType getConstraintType()
+    {
+        return UNARY_FUNCTION;
+    }
+
+    @Override
+    public String toString()
+    {
+        return function.getName() + "(" + columnName + ")";
+    }
+
+    public static class Serializer implements MetadataSerializer<UnaryFunctionColumnConstraint>
+    {
+        @Override
+        public void serialize(UnaryFunctionColumnConstraint columnConstraint, DataOutputPlus out, Version version) throws IOException
+        {
+            out.writeUTF(columnConstraint.function.getName());
+            out.writeUTF(columnConstraint.columnName.toCQLString());
+        }
+
+        @Override
+        public UnaryFunctionColumnConstraint deserialize(DataInputPlus in, Version version) throws IOException
+        {
+            String functionName = in.readUTF();
+            ConstraintFunction function;
+            String columnNameString = in.readUTF();
+            ColumnIdentifier columnName = new ColumnIdentifier(columnNameString, true);
+            try
+            {
+                function = createConstraintFunction(functionName, columnName);
+            }
+            catch (Exception e)
+            {
+                throw new IOException(e);
+            }
+
+            return new UnaryFunctionColumnConstraint(function, columnName);
+        }
+
+        @Override
+        public long serializedSize(UnaryFunctionColumnConstraint columnConstraint, Version version)
+        {
+            return TypeSizes.sizeof(columnConstraint.function.getClass().getName())
+                   + TypeSizes.sizeof(columnConstraint.columnName.toCQLString());
+        }
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o)
+            return true;
+
+        if (!(o instanceof UnaryFunctionColumnConstraint))
+            return false;
+
+        UnaryFunctionColumnConstraint other = (UnaryFunctionColumnConstraint) o;
+
+        return function.equals(other.function)
+               && columnName.equals(other.columnName);
+    }
+}
