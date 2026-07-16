205d4b2768d173c947d28105213eb11f57885aca
Jeremiah Jordan <jeremiah@apache.org>
2026-07-16 07:32:11 -0500

    Remove redundant intermediate casts in reflective type resolution

--- src/java/org/apache/cassandra/locator/AbstractReplicationStrategy.java
+++ src/java/org/apache/cassandra/locator/AbstractReplicationStrategy.java
@@ -429,9 +429,9 @@ public abstract class AbstractReplicationStrategy
 
         @SuppressWarnings("unchecked")
         Class<AbstractReplicationStrategy> strategyClass =
-            (Class<AbstractReplicationStrategy>)(Class<?>) FBUtilities.classForNameWithoutInitialization(className,
-                                                                                                        "replication strategy",
-                                                                                                        AbstractReplicationStrategy.class);
+            (Class<AbstractReplicationStrategy>) FBUtilities.classForNameWithoutInitialization(className,
+                                                                                             "replication strategy",
+                                                                                             AbstractReplicationStrategy.class);
         return strategyClass;
     }
 
